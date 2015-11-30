fce37eec89ab514515f103fdfbc30c07ed146d0d
Jack Somervell <jacksomervell@gmail.com>
2015-11-30 11:10:05 +0000

    added notes to functions

--- Front-end/js/imageController.js
+++ Front-end/js/imageController.js
@@ -11,21 +11,23 @@ angular
     self.data
     self.change
 
+//this calls the Flickr API and returns the data in JSON:
     self.getData = function(){
         $http.jsonp("http://api.flickr.com/services/feeds/photos_public.gne?tags=" + self.search + "&tagmode=all&format=json&jsoncallback=JSON_CALLBACK")
           .success(function(data) {
 
               self.data = data.items;
-              console.log(self.data)
-              console.log(self.search)
             })
 
   }
 
+//when the page loads, ensures some results display despite nothing being searched yet.
   if(self.search === undefined){
     self.search = "";
     self.getData();
   }
+
+ //is triggered when search text is inputted, calling the getData() function which makes the API request to Flickr:
 
     self.change = function(){
       setTimeout(self.getData(), 300)
